eafbd3ae6943120d8d568e8240c29fa86bad13e2
KwakuAldo <x.pac20032001@gmail.com>
2024-03-10 10:37:00 +0000

    make menu items functional

--- script/index.js
+++ script/index.js
@@ -7,9 +7,6 @@ const companyList = document.getElementById('company-li')
 
 function toggleMenu(anchor, listElement) {
     const img = anchor.querySelector('img')
-    // const currentSrc = img.getAttribute('src');
-    // const nextSrc = currentSrc === newSrc ? anchor.dataset.originalSrc : newSrc;
-    // img.setAttribute('src', nextSrc);
     if (img.src.includes('icon-arrow-down.svg')) {
         img.src = 'images/icon-arrow-up.svg';
     } else {
@@ -18,13 +15,48 @@ function toggleMenu(anchor, listElement) {
     listElement.classList.toggle('hidden')
 }
 
+function hideMenus() {
+    if (!featureList.classList.contains('hidden')) {
+        const featureBtnImg = featureBtn.querySelector('img')
+        featureBtnImg.src = 'images/icon-arrow-down.svg';
+        featureList.classList.add('hidden')
+    }
+    if (!companyList.classList.contains('hidden')) {
+        const companyBtnImg = companyBtn.querySelector('img')
+        companyBtnImg.src = 'images/icon-arrow-down.svg';
+        companyList.classList.add('hidden')
+    }
+}
+
 featureBtn.addEventListener('click', function() {
     toggleMenu(featureBtn, featureList)
+    if (!companyList.classList.contains('hidden')) {
+        const companyBtnImg = companyBtn.querySelector('img')
+        companyBtnImg.src = 'images/icon-arrow-down.svg';
+        companyList.classList.add('hidden')
+        hideMenus()
+    }
 })
 
 companyBtn.addEventListener('click', function () {
     toggleMenu(companyBtn, companyList);
+    if (!featureList.classList.contains('hidden')) {
+        const featureBtnImg = featureBtn.querySelector('img')
+        featureBtnImg.src = 'images/icon-arrow-down.svg';
+        featureList.classList.add('hidden')
+        hideMenus()
+    }
 });
+
+document.body.addEventListener('click', function(event) {
+    if (!event.target.closest('#dropDown-btn') 
+        && !event.target.closest('feature-li') 
+        && !event.target.closest('#dropDown-btn2') && !event.target.closest('company-li')) {
+            hideMenus()
+        }
+}) 
+
+
 
 // featureBtn.addEventListener('click', () => {
 //     featureList.classList.toggle('hidden')
